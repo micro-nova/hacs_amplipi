00769457468c6536bb666fb147868a033b979293
Steven Engelbert <steve@micro-nova.com>
2025-08-12 16:57:03 -0400

    remove unused import

--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 import pytest
-from pytest_socket import enable_socket, disable_socket, socket_allow_hosts
+from pytest_socket import enable_socket, socket_allow_hosts
 import json
 from homeassistant.core import HomeAssistant
 
